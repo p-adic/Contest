e8f05456df0c4e3d1087c9f3fe325b50ce03e7e8
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2024-11-27 11:23:06 +0900

    Add files via upload

--- Header.hpp
+++ Header.hpp
@@ -4,3 +4,5 @@
 #include "../Utility/Header.hpp"
 #include "../Utility/String/a_Body.hpp"
 #include "../Utility/VLArray/a_Body.hpp"
+#include "../Mathematics/Utility/Random/a_Body.hpp"
+#include "../Mathematics/Utility/Vector/a_Body.hpp"
