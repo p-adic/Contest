2e58ebbc7406fbb9d91858232a052854ea0c2d99
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2024-11-28 19:31:31 +0900

    Add files via upload

--- ProblemCreation/a_Body.hpp
+++ ProblemCreation/a_Body.hpp
@@ -9,19 +9,8 @@
 #include "WriteInSamples/a_Body.hpp"
 
 // "../../Utility/StdStream/a_Body.hpp"もincludeされる。
-#include "../../Utility/BinarySearch/a_Body.hpp"
-#include "../../Utility/TwoPointerApproach/a_Body.hpp"
-
-// データ構造用
-template <typename T> inline T Addition( const T& t0 , const T& t1 ) { return t0 + t1; }
-template <typename T> inline T Xor( const T& t0 , const T& t1 ){ return t0 ^ t1; }
-template <typename T> inline T Multiplication( const T& t0 , const T& t1 ) { return t0 * t1; }
-template <typename T> inline const T& Zero() { static const T z{}; return z; }
-template <typename T> inline const T& One() { static const T o = 1; return o; }
-template <typename T> inline T AdditionInv( const T& t ) { return -t; }
-template <typename T> inline T Id( const T& v ) { return v; }
-template <typename T> inline T Min( const T& a , const T& b ){ return a < b ? a : b; }
-template <typename T> inline T Max( const T& a , const T& b ){ return a < b ? b : a; }
+#include "../../Mathematics/Utility/BinarySearch/a_Body.hpp"
+#include "../../Mathematics/Utility/TwoPointerApproach/a_Body.hpp"
 
 // デバッグ用
 inline void AlertAbort( int n ) { CERR( "abort関数が呼ばれました。assertマクロのメッセージが出力されていない場合はオーバーフローの有無を確認をしてください。" ); }
