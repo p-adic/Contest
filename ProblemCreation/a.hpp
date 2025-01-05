d36481b9a75e010b7969c7a277308024ee335d8c
p進大好きbot <58817531+p-adic@users.noreply.github.com>
2025-01-05 17:51:01 +0900

    Add files via upload

--- ProblemCreation/a.hpp
+++ ProblemCreation/a.hpp
@@ -1,4 +1,4 @@
-// c:/Users/user/Documents/Programming/Contest/ProblemCreation/xa.hpp
+// c:/Users/user/Documents/Programming/Contest/ProblemCreation/a.hpp
 
 #pragma once
 #include "a_Macro.hpp"
@@ -8,3 +8,20 @@
 #include "RearrangeSamples/a.hpp"
 #include "WriteInSamples/a.hpp"
 
+#include "../../Mathematics/Utility/Set/a.hpp"
+#include "../../Mathematics/Utility/Tuple/a.hpp"
+#include "../../Mathematics/Utility/Vector/a.hpp"
+#include "../../Mathematics/Utility/Set/Map/a.hpp"
+#include "../../Utility/StdStream/a.hpp"
+#include "../../Utility/String/a.hpp"
+#include "../../Utility/String/ToArray/a.hpp"
+
+#include "../../Mathematics/Utility/BinarySearch/a.hpp"
+#include "../../Mathematics/Utility/TwoPointerApproach/a.hpp"
+#include "../../Mathematics/Algebra/Monoid/Group/Module/a.hpp"
+#include "../../Mathematics/Geometry/Graph/Grid/a.hpp"
+#include "../../Mathematics/Arithmetic/Mod/ConstexprModulo/a.hpp"
+#include "../../Mathematics/Combinatorial/Loop/a.hpp"
+
+CEXPR( uint , P , 998244353 );
+using MP = Mod<P>;
--- ProblemCreation/a_Body.hpp
+++ ProblemCreation/a_Body.hpp
@@ -8,9 +8,20 @@
 #include "RearrangeSamples/a_Body.hpp"
 #include "WriteInSamples/a_Body.hpp"
 
-// "../../Utility/StdStream/a_Body.hpp"もincludeされる。
+#include "../../Mathematics/Utility/Set/a_Body.hpp"
+#include "../../Mathematics/Utility/Tuple/a_Body.hpp"
+#include "../../Mathematics/Utility/Vector/a_Body.hpp"
+#include "../../Mathematics/Utility/Set/Map/a_Body.hpp"
+#include "../../Utility/StdStream/a_Body.hpp"
+#include "../../Utility/String/a_Body.hpp"
+#include "../../Utility/String/ToArray/a_Body.hpp"
+
 #include "../../Mathematics/Utility/BinarySearch/a_Body.hpp"
 #include "../../Mathematics/Utility/TwoPointerApproach/a_Body.hpp"
+#include "../../Mathematics/Algebra/Monoid/Group/Module/a_Body.hpp"
+#include "../../Mathematics/Geometry/Graph/Grid/a_Body.hpp"
+#include "../../Mathematics/Arithmetic/Mod/ConstexprModulo/a_Body.hpp"
+#include "../../Mathematics/Combinatorial/Loop/a_Body.hpp"
 
 // デバッグ用
 inline void AlertAbort( int n ) { CERR( "abort関数が呼ばれました。assertマクロのメッセージが出力されていない場合はオーバーフローの有無を確認をしてください。" ); }
